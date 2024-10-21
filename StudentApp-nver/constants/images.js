3d7555c7ae7f52b9f4f9d0ee87669df5b4afc7c6
eersay <105315644+eersay@users.noreply.github.com>
2024-10-21 23:19:47 +0530

    erasing old code

--- StudentApp-nver/constants/images.js
+++
@@ -1,9 +0,0 @@
-import logo from "../assets/images/logo.png";
-import logoGold from "../assets/images/logoGold.jpg"
-import logoSmall from "../assets/images/logoSmall.png";
-import profile from "../assets/images/profile.png";
-import openingScreen from "../assets/images/openingScreen.png";
-import signinbg1 from "../assets/images/signinbg1.jpeg";
-import emblem from "../assets/images/emblem.png";
-
-export default { profile,logo,logoGold,logoSmall,openingScreen,signinbg1,emblem, };
